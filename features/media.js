7dd6742ddf51867cbaa3e0b3788c0e5d731c8243
emre cavunt <emre.cavunt@gmail.com>
2018-11-17 22:51:50 +0300

    HooksRyancWU

---
+++ features/media.js
@@ -0,0 +1,34 @@
+import React,{useState,useEffect} from 'react';
+
+function useMedia(query){
+    let [matches,setMatches] = useState(
+        window.matchMedia(query).matches
+    );
+    //cdM, cdU
+    useEffect(()=>{
+        let media = window.matchMedia(query);
+        if(media.matches!==matches)
+        {
+            setMatches(media.matches);
+        }
+        let listener=()=>
+        setMatches(media.matches);
+        media.addListener(listener);
+        return()=>media.removeListener(listener);
+    },[query]);
+    return matches;   
+}
+
+function App(){
+    let small = useMedia("(max-width:400px)");
+    let large = useMedia("(max-width:800px)");
+    return(
+        <div className="Media">
+        <h1>Media</h1>
+        <p>Small ? {small ? "Yep": "Nope"}</p>
+        <p>Large ? {large ? "Yep": "Nope"}</p>
+        </div>
+    );
+}
+
+export default App;
